f2dea7931bcc1b43638bfc4f6bd4b3ae861b552d
sho Taguchi <s.taguchi0703@gmail.com>
2020-11-18 21:58:04 +0900

    init

--- django_initail_migrations.ps1
+++ django_initail_migrations.ps1
@@ -1,3 +1,3 @@
 docker-compose exec django python manage.py makemigrations
-exec django python manage.py migrate
-docker-compose exec django python manage.py createsuperuser
+docker-compose exec django python manage.py migrate
+# docker-compose exec django python manage.py createsuperuser
